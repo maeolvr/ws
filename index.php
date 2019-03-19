e764a6d4f04fe0752f052184065c87a0a376b022
maeolvr <43960170+maeolvr@users.noreply.github.com>
2019-03-19 09:26:49 +0100

    new 2

--- index.php
+++ index.php
@@ -5,13 +5,20 @@
     
 
     $sourceText = file_get_contents("https://api.lyrics.ovh/v1/".$artist."/".$title);
-    var_dump($sourceText);
-   
-
-   $sourceLang = $_POST['sourceLang'];
+    $sourceLang = $_POST['sourceLang'];
     $targetLang = $_POST['targetLang'];
     
     $res = file_get_contents("https://translate.googleapis.com/translate_a/single?client=gtx&sl="
         .$sourceLang."&tl=".$targetLang."&dt=t&q=".urlencode($sourceText));
+
     var_dump($res);
+    echo "<br>";
+    echo "<br>";
+    echo "<br>";
+    echo "<br>";
+    $res = explode('[',$res);
+    foreach($res as $lyrics){
+        print_r($lyrics);
+        echo "<br>";
+    }
 ?> 
