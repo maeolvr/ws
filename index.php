63f5df5d262e437b162e19bb7ce067b196ff7a50
cam-bdt <camillebourdet1999@gmail.com>
2019-03-19 10:21:22 +0100

    ghjk

--- index.php
+++ index.php
@@ -5,25 +5,35 @@
     
 
     $sourceText = file_get_contents("https://api.lyrics.ovh/v1/".$artist."/".$title);
-<<<<<<< HEAD
-    echo ($sourceText);
-   
-
-=======
->>>>>>> e764a6d4f04fe0752f052184065c87a0a376b022
     $sourceLang = $_POST['sourceLang'];
     $targetLang = $_POST['targetLang'];
     
+    var_dump($sourceText);
+    echo "<br>";
+    echo "<br>";
+    echo "<br>";
+    echo "<br>";
+
     $res = file_get_contents("https://translate.googleapis.com/translate_a/single?client=gtx&sl="
         .$sourceLang."&tl=".$targetLang."&dt=t&q=".urlencode($sourceText));
 
-    var_dump($res);
+/*      var_dump($res);
+ */     echo "<br>";
     echo "<br>";
     echo "<br>";
     echo "<br>";
+/*     $res = preg_split("[\][n]"."[\] [n]",$res);
+ */    
+$sourceText=explode('\n',$sourceText);
+ foreach($sourceText as $lyrics){
+        print_r($lyrics);
+        echo "<br>";
+    }
     echo "<br>";
-    $res = explode('[',$res);
-    foreach($res as $lyrics){
+    echo "<br>";
+    echo "<br>";
+    $res=explode('\\',$res);
+ foreach($res as $lyrics){
         print_r($lyrics);
         echo "<br>";
     }
