94fe6f1984ec75ca4ed0023919fdfe26705cdb71
maeolvr <43960170+maeolvr@users.noreply.github.com>
2019-03-19 09:10:47 +0100

    New

--- index.php
+++ index.php
@@ -1,14 +1,17 @@
 <?php
 
-    $artist = $_POST['artist'];
-    $title = $_POST['title'];
+    $artist = rawurlencode($_POST['artist']);
+    $title = rawurlencode($_POST['title']);
+    
 
-    $sourceText = file_get_contents('https://api.lyrics.ovh/v1/'.$artist.'/'.$title);
+    $sourceText = file_get_contents("https://api.lyrics.ovh/v1/".$artist."/".$title);
     var_dump($sourceText);
-    $sourceLang = $_POST['sourceLang'];
+   
+
+   $sourceLang = $_POST['sourceLang'];
     $targetLang = $_POST['targetLang'];
     
     $res = file_get_contents("https://translate.googleapis.com/translate_a/single?client=gtx&sl="
         .$sourceLang."&tl=".$targetLang."&dt=t&q=".urlencode($sourceText));
     var_dump($res);
-?>
+?> 
